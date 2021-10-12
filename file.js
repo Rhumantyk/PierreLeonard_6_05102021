60a8f54c44509a5c189e04518edf758cc926548b
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-10-12 14:03:13 +0200

    JS link-content fonctionnel

--- file.js
+++ file.js
@@ -1,18 +1,30 @@
 
-function myScrollFunc() // Scroll page
-{
-    myID = document.getElementById("#link-content"); 
-    let y = window.scrollY;
-    if (y >= 10)
-    {
-        myID.classList.add = "show";
-        myID.classList.remove = "hide";
-    }
-    else
-    {
-        myID.classList.add = "hide";
-        myID.classList.remove = "show";
-    }
-};
+// function myScrollFunc() // Scroll page
+// {
+//     myID = document.getElementById("#link-content"); 
+//     let y = window.scrollY;
+//     if (y >= 10)
+//     {
+//         myID.classList.add = "show";
+//         myID.classList.remove = "hide";
+//     }
+//     else
+//     {
+//         myID.classList.add = "hide";
+//         myID.classList.remove = "show";
+//     }
+// };
 
-window.addEventListener("scroll", myScrollFunc);
+// window.addEventListener("scroll", myScrollFunc);
+
+window.onscroll = function(e)
+{ 
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    var content = document.querySelector('#link-content');
+
+    scrollY <= this.lastScroll 
+      ? content.style.visibility = 'hidden'
+      : content.style.visibility = 'visible';
+
+    this.lastScroll = scrollY ;
+}
