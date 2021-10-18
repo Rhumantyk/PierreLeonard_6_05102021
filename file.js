c247e7b95d32798bb32c3fa0623162195b65fe49
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-10-18 14:35:58 +0200

    JS filtrage tag header terminé

--- file.js
+++ file.js
@@ -16,8 +16,6 @@ window.onscroll = function(e) // L'élément scroll est déclenché quand l'util
 
 
 // Clic sur #Tag = photographes ciblés apparaissent.
-
-// querySelectorAll("#mimi-keel, #nabeel-bradford", #etc ...);   --> Pourquoi ça ne fonctionne pas ?
 const ellieRoseWilkens = document.getElementById("ellie-rose-wilkens");
 const tracyGalindo = document.getElementById("tracy-galindo");
 const rhodeDubois = document.getElementById("rhode-dubois");
@@ -25,7 +23,7 @@ const marcelNikolic = document.getElementById("marcel-nikolic");
 const nabeelBradford = document.getElementById("nabeel-bradford");
 const mimiKeel = document.getElementById("mimi-keel");
 
-// #Portraits
+  // #Portraits
 const portraitTag = document.getElementById("portrait-tag");
 portraitTag.addEventListener("click", () =>
 {
@@ -37,7 +35,7 @@ portraitTag.addEventListener("click", () =>
   mimiKeel.style.display = "block";
 });
 
-// #Art
+  // #Art
 const artTag = document.getElementById("art-tag");
 artTag.addEventListener("click", () =>
 {
@@ -49,7 +47,7 @@ artTag.addEventListener("click", () =>
   tracyGalindo.style.display = "block";
 });
 
-// #Fashion
+  // #Fashion
 const fashionTag = document.getElementById("fashion-tag");
 fashionTag.addEventListener("click", () =>
 {
@@ -61,7 +59,7 @@ fashionTag.addEventListener("click", () =>
   tracyGalindo.style.display = "block";
 });
 
-// #Architecture
+  // #Architecture
 const architectureTag = document.getElementById("architecture-tag");
 architectureTag.addEventListener("click", () =>
 {
@@ -73,7 +71,7 @@ architectureTag.addEventListener("click", () =>
   tracyGalindo.style.display = "none";
 });
 
-// #Travel
+  // #Travel
 const travelTag = document.getElementById("travel-tag");
 travelTag.addEventListener("click", () =>
 {
@@ -85,7 +83,7 @@ travelTag.addEventListener("click", () =>
   tracyGalindo.style.display = "none";
 });
 
-// #Sport
+  // #Sport
 const sportTag = document.getElementById("sport-tag");
 sportTag.addEventListener("click", () =>
 {
@@ -97,93 +95,26 @@ sportTag.addEventListener("click", () =>
   tracyGalindo.style.display = "none";
 });
 
+  // #Animals
+const animalsTag = document.getElementById("animals-tag");
+animalsTag.addEventListener("click", () =>
+{
+  ellieRoseWilkens.style.display = "none";
+  rhodeDubois.style.display = "block";
+  marcelNikolic.style.display = "none";
+  nabeelBradford.style.display = "none";
+  mimiKeel.style.display = "block";
+  tracyGalindo.style.display = "none";
+});
 
-// const artTag = document.getElementById("art-tag"); // tagBtn#
-// artTag.onclick = function ()
-// {
-//   if (ellieRoseWilkens.style.display !== "none")
-//   {
-//     ellieRoseWilkens.style.display = "none";
-//     rhodeDubois.style.display = "none";
-//     marcelNikolic.style.display = "none";
-//     nabeelBradford.style.display = "none";
-//     mimiKeel.style.display = "none";
-//   }
-//   else
-//   {
-//     tracyGalindo.style.display = "block";
-//   }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Lien : https://codepen.io/leandrino/pen/bdEajV
-// var items = document.querySelectorAll('#photographers div');
-
-// // Filtrage au clic
-// each('.nav-header a', function(el)
-// {
-//   el.addEventListener('click', function(e)
-//   {
-//     e.preventDefault();
-//     filterLinks(el);
-//   });
-// });
-
-// // filter links functions
-// function filterLinks(element)
-// {
-//   // get text 
-//   var el = element.textContent,
-//     // convert to lowercase
-//     linksTolowerCase = el.toLowerCase();
-//   // if all remove all elements
-//   if (el === 'All')
-//   {
-//     // first show all view class
-//     each('.view', function(e)
-//     {
-//       e.classList.remove('view');
-//     });
-//   } else {
-//     // if not click all remove all elements
-//     each('.view', function(e)
-//     {
-//       e.classList.remove('view');
-//     });
-//   }
-// };
-// // forech arrays
-// function each(el, callback)
-// {
-//   var allDivs = document.querySelectorAll(el),
-//     alltoArr = Array.prototype.slice.call(allDivs);
-//   Array.prototype.forEach.call(alltoArr, function(selector, index)
-//   {
-//     if (callback) return callback(selector);
-//   });
-// };
+  // #Events
+const eventsTag = document.getElementById("events-tag");
+eventsTag.addEventListener("click", () =>
+{
+  ellieRoseWilkens.style.display = "none";
+  rhodeDubois.style.display = "block";
+  marcelNikolic.style.display = "none";
+  nabeelBradford.style.display = "none";
+  mimiKeel.style.display = "block";
+  tracyGalindo.style.display = "block";
+});
