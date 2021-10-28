1f6a8524f804b99ec15eb786ec2fb881fce7ee4f
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-10-29 01:23:15 +0200

    Tentative ajout autre page HTML via JS

--- Page_Photographes/file_photographers.js
+++ Page_Photographes/file_photographers.js
@@ -35,6 +35,46 @@ const data = fetchData(file).then((data) => // Puisqu'il y a une function async,
       photographersDetails.classList.add('photographers-details'); // Ajout de la classe correspondante.
       contactDetails.appendChild(photographersDetails); // Appartient à la div contactDetails.
 
+      // Page Mimi Keel
+      if (window.location.pathname =='/Page_Photographes/mimi_keel.html')
+      {
+          photographersDetails.innerHTML = 
+          `
+          <h1>${data.photographers[0].name}</h1>
+          <p>${data.photographers[0].city}, ${data.photographers[0].country}</p>
+          <p>${data.photographers[0].tagline}</p>
+          `
+
+          // Tags + Suppression virgules contenues dans la liste JSON de "tags" [] + Ajout individuel sans boucle forEach.
+          const tagsFiltered = document.createElement('div'); // Création de div tags-filtered.
+          tagsFiltered.classList.add('tags-filtered'); // Ajout de la classe correspondante.
+          photographersDetails.appendChild(tagsFiltered); // Appartient à la div contactDetails.
+          tagsFiltered.innerHTML +=
+            `<a href="#" class="nav-filters">
+            #${data.photographers[1].tags[0]}</a>
+            <span class="screenreader-text">#${data.photographers[1].tags[0]}</span>`
+            +
+            `<a href="#" class="nav-filters">
+            #${data.photographers[1].tags[1]}</a>
+            <span class="screenreader-text">#${data.photographers[1].tags[1]}</span>`;
+          
+          // Ajout bouton "Contactez-moi".
+          const btnContact = document.createElement('button'); // Création de button.
+          btnContact.setAttribute('role', 'button'); // Ajout du rôle correspondant.
+          photographersDetails.appendChild(btnContact); // Appartient à la div contactDetails.
+          btnContact.innerHTML = `Contactez-moi`;
+
+      // Ajout photo photographe
+      const picturePhotographer = document.createElement('div'); // Création de div div-photo.
+      picturePhotographer.classList.add('div-photo'); // Ajout de la classe correspondante.
+      contactDetails.appendChild(picturePhotographer); // Appartient à la div contactDetails.
+      picturePhotographer.innerHTML =
+      `
+      <img src="/Photos_FishEye/Sample_Photos/Photographers_ID_Photos/${data.photographers[1].portrait}" alt="${data.photographers[1].name}" class="img-pictures">
+      `;
+      }
+
+      // Page Ellie-Rose_Wilkens
       if (window.location.pathname =='/Page_Photographes/Ellie-Rose_Wilkens.html')
       {
           photographersDetails.innerHTML = 
