06d6ab22c0374d454347fa311cb710934a3bd172
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-10-29 02:27:40 +0200

    Ajout photographe 5

--- Page_Photographes/file_photographers.js
+++ Page_Photographes/file_photographers.js
@@ -203,5 +203,52 @@ const data = fetchData(file).then((data) => // Puisqu'il y a une function async,
       <img src="/Photos_FishEye/Sample_Photos/Photographers_ID_Photos/${data.photographers[3].portrait}" alt="${data.photographers[3].name}" class="img-pictures">
       `;
       }
+
+      // Page Rhode Dubois
+      if (window.location.pathname =='/Page_Photographes/Rhode_Dubois.html')
+      {
+          photographersDetails.innerHTML = 
+          `
+          <h1>${data.photographers[4].name}</h1>
+          <p>${data.photographers[4].city}, ${data.photographers[4].country}</p>
+          <p>${data.photographers[4].tagline}</p>
+          `
+
+          // Tags + Suppression virgules contenues dans la liste JSON de "tags" [] + Ajout individuel sans boucle forEach.
+          const tagsFiltered = document.createElement('div'); // Création de div tags-filtered.
+          tagsFiltered.classList.add('tags-filtered'); // Ajout de la classe correspondante.
+          photographersDetails.appendChild(tagsFiltered); // Appartient à la div contactDetails.
+          tagsFiltered.innerHTML +=
+            `<a href="#" class="nav-filters">
+            #${data.photographers[4].tags[0]}</a>
+            <span class="screenreader-text">#${data.photographers[4].tags[0]}</span>`
+            +
+            `<a href="#" class="nav-filters">
+            #${data.photographers[4].tags[1]}</a>
+            <span class="screenreader-text">#${data.photographers[4].tags[1]}</span>`
+            +
+            `<a href="#" class="nav-filters">
+            #${data.photographers[4].tags[2]}</a>
+            <span class="screenreader-text">#${data.photographers[4].tags[2]}</span>`
+            +
+            `<a href="#" class="nav-filters">
+            #${data.photographers[4].tags[3]}</a>
+            <span class="screenreader-text">#${data.photographers[4].tags[3]}</span>`;
+          
+          // Ajout bouton "Contactez-moi".
+          const btnContact = document.createElement('button'); // Création de button.
+          btnContact.setAttribute('role', 'button'); // Ajout du rôle correspondant.
+          photographersDetails.appendChild(btnContact); // Appartient à la div contactDetails.
+          btnContact.innerHTML = `Contactez-moi`;
+
+      // Ajout photo photographe
+      const picturePhotographer = document.createElement('div'); // Création de div div-photo.
+      picturePhotographer.classList.add('div-photo'); // Ajout de la classe correspondante.
+      contactDetails.appendChild(picturePhotographer); // Appartient à la div contactDetails.
+      picturePhotographer.innerHTML =
+      `
+      <img src="/Photos_FishEye/Sample_Photos/Photographers_ID_Photos/${data.photographers[4].portrait}" alt="${data.photographers[4].name}" class="img-pictures">
+      `;
+      }
 });
 console.log(data);
