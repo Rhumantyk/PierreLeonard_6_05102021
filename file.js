08db7113ac715916ee933de7f498525072ce534d
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-10-30 21:35:18 +0200

    Triage tags header

--- file.js
+++ file.js
@@ -31,13 +31,19 @@ async function fetchData(file)
 // Main div contenant les div des photographes.
 const divPhotographers = document.getElementById('photographers');
 
+// const ellieRoseWilkens = document.getElementById("photographers").children[1].style.display = "none";
+// divPhotographers.children[1].style.display = "display";
+
 // Traphotographersent des données
 const data = fetchData(file).then((data) => // Puisqu'il y a une function async, .then sert à en attendre la réponse.
 {
+
 //   // Version initiale manuelle à supprimer plus tard
 
 //   // Clic sur #Tag = photographes ciblés apparaissent.
-//   const ellieRoseWilkens = document.getElementById("ellie-rose-wilkens");
+                  // const ellieRoseWilkens = document.getElementById("photographers").children[0];
+                  // ellieRoseWilkens.style.display = "none";
+// ellieRoseWilkens= document.getElementById("ellie-rose_wilens");
 //   const tracyGalindo = document.getElementById("tracy-galindo");
 //   const rhodeDubois = document.getElementById("rhode-dubois");
 //   const marcelNikolic = document.getElementById("marcel-nikolic");
@@ -48,7 +54,7 @@ const data = fetchData(file).then((data) => // Puisqu'il y a une function async,
 //   const portraitTag = document.getElementById("portrait-tag");
 //   portraitTag.addEventListener("click", () =>
 //   {
-//     ellieRoseWilkens.style.display = "none";
+    // ellieRoseWilkens.style.display = "none";
 //     tracyGalindo.style.display = "none";
 //     rhodeDubois.style.display = "none";
 //     marcelNikolic.style.display = "none";
@@ -172,81 +178,130 @@ const data = fetchData(file).then((data) => // Puisqu'il y a une function async,
         tagsFiltered.innerHTML += `<a href="#" class="nav-filters">#${tag}</a>`; // Ajout HTML. ${tag} seul puisque string.
       });
   });
+
+  // Filtre des #Tags
+
+  // function tagsClic(tag)
+  // {
+  //   let photographers = document.getElementById("photographers");
+  //   if (tag.id == photographers:nth-child(1))
+  //   {
+  //     document.getElementById("userinfo").style.display="block";
+  //   } 
+  //   else
+  //   {
+  //     document.getElementById("userinfo").style.display="none";
+  //   }
+  // }
+  // console.log(tagsClic);
+
+  const ellieRoseWilkens = divPhotographers.children[0];
+  const mimiKeel = divPhotographers.children[1];
+  const tracyGalindo = divPhotographers.children[2];
+  const nabeelBradford = divPhotographers.children[3];
+  const rhodeDubois = divPhotographers.children[4];
+  const marcelNikolic = divPhotographers.children[5];
+  // ellieRoseWilkens.style.display = "none";
+
+      // #Portraits
+  const portraitTag = document.getElementById("portrait-tag");
+  portraitTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    tracyGalindo.style.display = "none";
+    rhodeDubois.style.display = "none";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "block";
+    mimiKeel.style.display = "block";
+  });
+
+    // #Art
+  const artTag = document.getElementById("art-tag");
+  artTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    rhodeDubois.style.display = "none";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "none";
+    tracyGalindo.style.display = "block";
+  });
+
+    // #Fashion
+  const fashionTag = document.getElementById("fashion-tag");
+  fashionTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    rhodeDubois.style.display = "block";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "none";
+    tracyGalindo.style.display = "block";
+  });
+
+    // #Architecture
+  const architectureTag = document.getElementById("architecture-tag");
+  architectureTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "block";
+    rhodeDubois.style.display = "none";
+    marcelNikolic.style.display = "block";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "none";
+    tracyGalindo.style.display = "none";
+  });
+
+    // #Travel
+  const travelTag = document.getElementById("travel-tag");
+  travelTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    rhodeDubois.style.display = "none";
+    marcelNikolic.style.display = "block";
+    nabeelBradford.style.display = "block";
+    mimiKeel.style.display = "block";
+    tracyGalindo.style.display = "none";
+  });
+
+    // #Sport
+  const sportTag = document.getElementById("sport-tag");
+  sportTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "block";
+    rhodeDubois.style.display = "block";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "none";
+    tracyGalindo.style.display = "none";
+  });
+
+    // #Animals
+  const animalsTag = document.getElementById("animals-tag");
+  animalsTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    rhodeDubois.style.display = "block";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "block";
+    tracyGalindo.style.display = "none";
+  });
+
+    // #Events
+  const eventsTag = document.getElementById("events-tag");
+  eventsTag.addEventListener("click", () =>
+  {
+    ellieRoseWilkens.style.display = "none";
+    rhodeDubois.style.display = "block";
+    marcelNikolic.style.display = "none";
+    nabeelBradford.style.display = "none";
+    mimiKeel.style.display = "block";
+    tracyGalindo.style.display = "block";
+  });
+
+  // if(tag.id == ellie)
+  // {
+  //   ellie.style.display = "none";
+  // }
 });
 console.log(data);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
